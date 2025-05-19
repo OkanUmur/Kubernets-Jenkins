d64ea56023347da834978b6e371e6226138c0833
OkanUmur <umuro2124@gmail.com>
2025-05-19 17:45:45 +0300

    Initial commit

---
+++ src/main/java/com/example/kubernets/KubernetsApplication.java
@@ -0,0 +1,11 @@
+package com.example.kubernets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetsApplication.class, args);
+    }
+}
